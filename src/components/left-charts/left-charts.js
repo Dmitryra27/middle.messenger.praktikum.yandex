c60e1c61bebc7713a9be5a2e9be37c8884f18ee4
dmitry sumanov <dmitryra27@gmail.com>
2023-01-15 20:55:19 +0300

    Sprint 1

--- src/components/left-charts/left-charts.js
+++ src/components/left-charts/left-charts.js
@@ -4,7 +4,7 @@ import Chart from '../chart/chart';
 function LeftCharts(charts) {
 
     return (`<div class="left-charts-box">
-            <button class="chart-profile">Профиль></button>
+            <button class="chart-profile" onclick="window.location.href = '/profile'" type="button">Профиль></button>
             <input name='search' value="Поиск">
             <div class="left-charts-box__child">${Chart(charts[0])}</div>
             <div class="left-charts-box__child">${Chart(charts[1])}</div>
