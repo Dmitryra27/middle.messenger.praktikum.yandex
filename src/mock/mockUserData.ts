2b006c30599d4f452fac81b6483d0afc1366bcc9
dmitry sumanov <dmitryra27@gmail.com>
2023-02-04 13:33:11 +0300

    first commit sprint_2

---
+++ src/mock/mockUserData.ts
@@ -0,0 +1,12 @@
+// это тест для того, чтобы сделать Pullrequest
+
+export const userData = {
+    "id": 123,
+    "first_name": "Petya",
+    "second_name": "Pupkin",
+    "display_name": "Petya Pupkin",
+    "login": "userLogin",
+    "email": "my@email.com",
+    "phone": "89223332211",
+    "avatar": "../../static/images/avatar12.jpg"
+};
