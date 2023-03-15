2eac6957c5874ea4923a02c8218907ee7bd02eef
dmitry sumanov <dmitryra27@gmail.com>
2023-03-15 19:09:15 +0300

    Sprint_4 with webpack

--- src/components/form/form.ts
+++ src/components/form/form.ts
@@ -1,10 +1,10 @@
 import Block from "../../core/Block";
-import {validate} from "../../utils/validateInput";
+import {validate, validEvents} from "../../utils/validateInput";
 //import ErrorInput from "../ErrorInput/errorInput";
-//import Input from "../input/index";
 import template from "./form.hbs";
+import Input from '../input';
 //import {default as styles} from "./formInput.module.scss";
-import * as styles from  "./form.module.scss";
+//import * as styles from  "./form.module.scss";
 
 interface FormInputProps {
   label?: string,
@@ -20,8 +20,31 @@ class FormInput extends Block {
     super(props);
   }
 
+  protected componentDidUpdate(_oldProps: FormInputProps, newProps: FormInputProps): boolean {
+    (this.children.input as Block).setProps({value: newProps.validation.value});
+
+    return false;
+  }
+
+  init() {
+    const {label, type, name, placeholder, validation} = this.props;
+    //this.children.error = new ErrorInput({
+    // text: "",
+    //});
+    this.children.input = new Input({
+      label,
+      type,
+      name,
+      placeholder,
+      value: validation.value,
+      events: validEvents(validation),
+    });
+
+    //validation.errorComponent = this.children.error;
+  }
+
   render() {
-    return this.compile(template, {...this.props, styles});
+    return this.compile(template, {...this.props});
   }
 }
 
